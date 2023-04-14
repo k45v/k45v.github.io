d49eab1ea7ddd59b97f49ea133790917ec459ce8
k45v <2790182+k45v@users.noreply.github.com>
2023-04-14 04:40:51 +0000

    Deploying to gh-pages from @ k45v/k45v.github.io@da1956efdde35d0ef846f25afa0b15b2ec6811b7 🚀

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -12,7 +12,6 @@ let toggleTheme = (theme) => {
 let setTheme = (theme) =>  {
   transTheme();
   setHighlight(theme);
-  setGiscusTheme(theme);
 
   if (theme) {
     document.documentElement.setAttribute("data-theme", theme);
@@ -41,24 +40,6 @@ let setHighlight = (theme) => {
     document.getElementById("highlight_theme_light").media = "";
   }
 }
-
-
-let setGiscusTheme = (theme) => {
-
-  function sendMessage(message) {
-    const iframe = document.querySelector('iframe.giscus-frame');
-    if (!iframe) return;
-    iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
-  }
-
-  sendMessage({
-    setConfig: {
-      theme: theme
-    }
-  });
-
-}
-
 
 let transTheme = () => {
   document.documentElement.classList.add("transition");
